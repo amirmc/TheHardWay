7f48f38b6cd254a5c5f3754b9d2a572003ad1c17
Amir Chaudhry <amirmc@gmail.com>
2011-09-24 18:22:39 +0100

    na na na na nana na, getting stringy wit' it, na na na na nana na

--- ex5v2.py
+++ ex5v2.py
@@ -1,18 +1,24 @@
-my_name = 'Amir Chaudhry'
-my_age = 31 # not a lie
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Amir Chaudhry'
+age = 31.0 # not a lie
+height_cm = 175.0 # cm
+weight_kg = 76.0 # kgs
+eyes = 'brown'
+teeth = 'white'
+hair = 'black'
 
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+height_in = height_cm*0.3937008
+weight_lb = weight_kg*2.20462
+
+print "Let's talk about %s." % name
+print "He's %d cm tall." % height_cm
+print "That's", height_in, "in imperial units"
+print "That's {0:g} inches, in imperial units" .format(height_in)
+print "He's %d kgs heavy." % weight_kg
+print "Also known as {0:g}lbs." .format(weight_lb)
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
-print "If I add {0:d}, {1:d}, and {2:d} I get {3:d}.".format(my_age, my_height, my_weight,
-                                                             my_age + my_height + my_weight)
+print "If I add {0:g}, {1:g}, and {2:g} I get {3:g}.".format(age, height_cm, weight_kg,
+                                                             age + height_cm + weight_kg)
